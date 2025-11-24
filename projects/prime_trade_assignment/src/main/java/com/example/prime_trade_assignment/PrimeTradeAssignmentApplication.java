ce03baaf8f07297ff5bc68c89ef2d47c77019150
saikad27 <sairajkad501@icloud.com>
2025-11-24 16:47:15 +0530

    first_commit

---
+++ projects/prime_trade_assignment/src/main/java/com/example/prime_trade_assignment/PrimeTradeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.prime_trade_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeTradeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeTradeAssignmentApplication.class, args);
+	}
+
+}
